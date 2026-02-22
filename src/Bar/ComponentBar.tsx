cf86f12512ad298e70c85632cee0ad8e46197996
Ra <68011480@kmitl.ac.th>
2026-02-22 15:01:02 +0700

    Initial commit

---
+++ src/Bar/ComponentBar.tsx
@@ -0,0 +1,28 @@
+import myImage from "../assets/logo-512.png";
+
+function Bar() {
+    
+  return (
+    <div className="sidebar">
+      <div className="bg-dark">
+        <img src={myImage} alt="My picture" />
+      </div>
+    </div>
+  );
+}
+
+export default Bar;
+
+{
+  /* <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Enable body scrolling</button>
+
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <p>Try scrolling the rest of the page to see this option in action.</p>
+  </div>
+</div> */
+}
